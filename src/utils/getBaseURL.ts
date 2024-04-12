4bd3954c54e5f127454b707773b7ddefedba94a1
kai-nakao <nkkai0803@gmail.com>
2024-04-12 16:52:39 +0900

    fix: auto env url

--- src/utils/getBaseURL.ts
+++ src/utils/getBaseURL.ts
@@ -2,10 +2,15 @@ export function getBaseUrl() {
   if (typeof window !== 'undefined')
     // browser should use relative path
     return ''
+  console.log(
+    'process.env.NEXT_PUBLIC_VERCEL_ENV',
+    process.env.NEXT_PUBLIC_VERCEL_ENV,
+  )
+  console.log('process.env.VERCEL_ENV', process.env.VERCEL_ENV)
 
-  switch (process.env.NEXT_PUBLIC_VERCEL_ENV) {
+  switch (process.env.VERCEL_ENV) {
     case 'preview':
-      return `https://${process.env.NEXT_PUBLIC_VERCEL_ENV}`
+      return `https://${process.env.VERCEL_URL}`
     case 'production':
       return `https://${process.env.PRODUCTION_URL}`
     default:
