6ad0fe4774bef2e812db71d4cc317642977bd0f8
kai-nakao <nkkai0803@gmail.com>
2024-04-12 15:47:03 +0900

    fix: endpoint url

--- src/utils/getBaseURL.ts
+++ src/utils/getBaseURL.ts
@@ -2,9 +2,13 @@ export function getBaseUrl() {
   if (typeof window !== 'undefined')
     // browser should use relative path
     return ''
-  if (process.env.NEXT_PUBLIC_VERCEL_URL)
-    // reference for vercel.com
-    return `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
-  // assume localhost
-  return `http://localhost:${process.env.PORT ?? 3000}`
+
+  switch (process.env.VERCEL_ENV) {
+    case 'preview':
+      return `https://${process.env.NEXT_PUBLIC_VERCEL_ENV}`
+    case 'production':
+      return `https://${process.env.PRODUCTION_URL}`
+    default:
+      return 'http://localhost:3000'
+  }
 }
