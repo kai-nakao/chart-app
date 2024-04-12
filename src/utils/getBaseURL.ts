1c24f2ec4aef2bbc2ba245c91743298595cd2f9e
kai-nakao <nkkai0803@gmail.com>
2024-04-12 18:27:00 +0900

    fix: adapt system env value

--- src/utils/getBaseURL.ts
+++ src/utils/getBaseURL.ts
@@ -1,19 +1,10 @@
 export function getBaseUrl() {
-  if (typeof window !== 'undefined')
-    // browser should use relative path
-    return ''
-  console.log(
-    'process.env.NEXT_PUBLIC_VERCEL_ENV',
-    process.env.NEXT_PUBLIC_VERCEL_ENV,
-  )
-  console.log('process.env.VERCEL_ENV', process.env.VERCEL_ENV)
-
   switch (process.env.VERCEL_ENV) {
-    case 'preview':
-      return `https://${process.env.VERCEL_URL}`
+    case 'development':
+      return 'http://localhost:3000'
     case 'production':
       return `https://${process.env.PRODUCTION_URL}`
     default:
-      return 'http://localhost:3000'
+      return `https://${process.env.VERCEL_URL}`
   }
 }
