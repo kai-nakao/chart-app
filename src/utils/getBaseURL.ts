e2f589bda21cf35f621c1ec8e3b4435ce994fc5f
kai-nakao <nkkai0803@gmail.com>
2024-04-12 15:51:32 +0900

    fix: NEXT_PUBLIC env

--- src/utils/getBaseURL.ts
+++ src/utils/getBaseURL.ts
@@ -3,7 +3,7 @@ export function getBaseUrl() {
     // browser should use relative path
     return ''
 
-  switch (process.env.VERCEL_ENV) {
+  switch (process.env.NEXT_PUBLIC_VERCEL_ENV) {
     case 'preview':
       return `https://${process.env.NEXT_PUBLIC_VERCEL_ENV}`
     case 'production':
