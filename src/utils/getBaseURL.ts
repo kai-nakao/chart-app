f5d4792e500823040a195ffcbfad796a66022ca2
kai-nakao <nkkai0803@gmail.com>
2024-04-13 14:27:58 +0900

    add log

--- src/utils/getBaseURL.ts
+++ src/utils/getBaseURL.ts
@@ -1,5 +1,8 @@
 export function getBaseUrl() {
   console.log('process.env.VERCEL_BRANCH_URL: ', process.env.VERCEL_BRANCH_URL)
+  console.log('process.env.VERCEL_URL', process.env.VERCEL_URL)
+  console.log('process.env.PRODUCTION_URL', process.env.PRODUCTION_URL)
+  console.log('process.env.VERCEL_ENV', process.env.VERCEL_ENV)
   switch (process.env.VERCEL_ENV) {
     case 'development':
       return 'http://localhost:3000'
